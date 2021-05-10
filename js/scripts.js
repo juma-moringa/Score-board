afbf9d883f745f890357ba9478ef5a107d96cfcb
juma-moringa <allan.juma@student.moringaschool.com>
2021-05-10 10:07:51 +0300

    updated the js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Business Logic
 var sum = 0;
 function scoreBoard(userValue) {
-  var questions = ["js", "year", "framework","function"];
+  var questions = [ "framework" ,"function" ,"year","js" ];
   questions.forEach(function(question) {
     var userValue = $("input:radio[name=" + question + "]:checked").val();
     if(userValue === "10") {
@@ -9,3 +9,23 @@ function scoreBoard(userValue) {
     }
   });
 }
+
+// User Interface.
+ 
+  $("#submit").click(function(event) {
+    event.preventDefault();
+    $("#score").show();
+    $("#form1").hide();
+    // scoreCalculator();
+    // var output= $("#").val();
+    if(sum <= 30) {
+      $(".name-display").text(`Nice trial`);
+    } else {
+      $(".name-display").text(`Congratulations.`);
+    }
+    $(".scoreDisplay").text(sum + "/40");
+  })
+  $("#retake").click(function(event) {
+    event.preventDefault();
+    location.reload();
+  })
